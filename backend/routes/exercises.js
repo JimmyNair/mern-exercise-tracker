5adf37d821f9018db62f7486543a138eab103794
James Nair <vanairngo2@hotmail.com>
2020-01-10 10:55:34 +1000

    add more route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,15 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Exercise delete"))
+        .catch(err => res.status(400).json("Error: ") + err);
+});
 module.exports = router;
